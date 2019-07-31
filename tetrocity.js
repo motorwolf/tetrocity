1ae50bc9e5c093576bc7ec25ff61f6b314f811f7
Kristen Payne <kristen@polysign.io>
2019-07-31 15:34:50 -0700

    Fix formatting

--- tetrocity.js
+++ tetrocity.js
@@ -2,49 +2,23 @@ var HEIGHT = 25;
 var WIDTH = 10;
 var STARTING_POSITION = Math.floor((WIDTH - 1) / 2);
 var SHAPES = [
-    { type: "square",
-        diagram: ["11", "11"]
-    },
-    { type: "rightL",
-        diagram: ["1.", "1.", "11"]
-    },
-    { type: "leftL",
-        diagram: [".1",
-            ".1",
-            "11",
-        ]
-    },
-    { type: 'rightZ',
-        diagram: ["1.",
-            "11",
-            ".1",
-        ]
-    },
-    { type: "leftZ",
-        diagram: [".1",
-            "11",
-            "1."
-        ]
-    },
-    { type: 'line',
-        diagram: ["1", "1", "1", "1"]
-    },
-    { type: 'prong',
-        diagram: [
-            ".1.",
-            "111",
-        ]
-    }
+    { type: "square", diagram: ["11", "11"] },
+    { type: "rightL", diagram: ["1.", "1.", "11"] },
+    { type: "leftL", diagram: [".1", ".1", "11"] },
+    { type: "rightZ", diagram: ["1.", "11", ".1"] },
+    { type: "leftZ", diagram: [".1", "11", "1."] },
+    { type: "line", diagram: ["1", "1", "1", "1"] },
+    { type: "prong", diagram: [".1.", "111"] }
 ];
 function htmlRender(game) {
-    var gameboard = document.getElementById('gameboard');
-    gameboard.innerHTML = '';
+    var gameboard = document.getElementById("gameboard");
+    gameboard.innerHTML = "";
     for (var r = 0; r < game.getHeight(); r++) {
-        var row = document.createElement('div');
-        row.className = 'row';
+        var row = document.createElement("div");
+        row.className = "row";
         for (var c = 0; c < game.getWidth(); c++) {
-            var cell = document.createElement('div');
-            cell.className = game.isFilled(r, c) ? 'cell filled' : 'cell';
+            var cell = document.createElement("div");
+            cell.className = game.isFilled(r, c) ? "cell filled" : "cell";
             row.appendChild(cell);
         }
         gameboard.appendChild(row);
@@ -53,9 +27,9 @@ function htmlRender(game) {
 function consoleRender(game) {
     console.log("==========================");
     for (var r = 0; r < game.getHeight(); r++) {
-        var row = '';
+        var row = "";
         for (var c = 0; c < game.getWidth(); c++) {
-            row += game.isFilled(r, c) ? '[]' : '00';
+            row += game.isFilled(r, c) ? "[]" : "00";
         }
         console.log(row);
     }
@@ -86,28 +60,31 @@ var Game = /** @class */ (function () {
         return this.interval;
     };
     Game.prototype.canPieceAdvance = function (direction) {
+        if (direction === void 0) { direction = undefined; }
         var row = this.pieceRow;
         var col = this.pieceCol;
         switch (direction) {
-            case 'down':
+            case "down":
                 row++;
                 break;
-            case 'left':
+            case "left":
                 col--;
                 break;
-            case 'right':
+            case "right":
                 col++;
                 break;
             default:
                 break;
         }
         //boundary check
-        if (row + this.shape.length > HEIGHT || col < 0 || col + this.shape[0].length > WIDTH) {
+        if (row + this.shape.length > HEIGHT ||
+            col < 0 ||
+            col + this.shape[0].length > WIDTH) {
             return false;
         }
         for (var r = 0; r < this.shape.length; r++) {
             for (var c = 0; c < this.shape[0].length; c++) {
-                if (this.grid[row + r][col + c] === 1 && this.shape[r][c] === '1') {
+                if (this.grid[row + r][col + c] === 1 && this.shape[r][c] === "1") {
                     return false;
                 }
             }
@@ -117,7 +94,7 @@ var Game = /** @class */ (function () {
     Game.prototype.fill = function () {
         for (var r = 0; r < this.shape.length; r++) {
             for (var c = 0; c < this.shape[0].length; c++) {
-                if (this.shape[r][c] === '1') {
+                if (this.shape[r][c] === "1") {
                     this.grid[this.pieceRow + r][this.pieceCol + c] = 1;
                 }
             }
@@ -134,7 +111,7 @@ var Game = /** @class */ (function () {
             return true;
         if (r >= this.pieceRow && r < this.pieceRow + this.shape.length) {
             if (c >= this.pieceCol && c < this.pieceCol + this.shape[0].length) {
-                return this.shape[r - this.pieceRow][c - this.pieceCol] === '1';
+                return this.shape[r - this.pieceRow][c - this.pieceCol] === "1";
             }
         }
         return false;
@@ -143,8 +120,8 @@ var Game = /** @class */ (function () {
         // Currently this only rotates to the right
         var oldShape = this.shape.slice();
         var newShape = [];
-        while (oldShape[0] !== '') {
-            var newShapeLine = '';
+        while (oldShape[0] !== "") {
+            var newShapeLine = "";
             for (var r = oldShape.length - 1; r > -1; r--) {
                 newShapeLine += oldShape[r][0];
                 oldShape[r] = oldShape[r].slice(1);
@@ -152,20 +129,20 @@ var Game = /** @class */ (function () {
             newShape.push(newShapeLine);
         }
         this.shape = newShape;
-        if ((this.pieceCol + this.shape[0].length) >= WIDTH - 1) {
+        if (this.pieceCol + this.shape[0].length >= WIDTH - 1) {
             this.pieceCol = WIDTH - this.shape[0].length;
         }
-        if (!this.canPieceAdvance(null))
+        if (!this.canPieceAdvance())
             this.shape = oldShape;
     };
     Game.prototype.bigDrop = function () {
-        while (this.canPieceAdvance('down')) {
+        while (this.canPieceAdvance("down")) {
             this.pieceRow++;
         }
         this.fill();
     };
     Game.prototype.dropPiece = function () {
-        if (this.canPieceAdvance('down')) {
+        if (this.canPieceAdvance("down")) {
             this.pieceRow++;
         }
         else {
@@ -175,13 +152,13 @@ var Game = /** @class */ (function () {
     Game.prototype.movePiece = function (direction) {
         if (this.canPieceAdvance(direction)) {
             switch (direction) {
-                case 'down':
+                case "down":
                     this.pieceRow++;
                     break;
-                case 'left':
+                case "left":
                     this.pieceCol--;
                     break;
-                case 'right':
+                case "right":
                     this.pieceCol++;
                     break;
                 default:
@@ -214,19 +191,19 @@ var Game = /** @class */ (function () {
     return Game;
 }());
 function browserGame() {
-    var game = new Game;
-    document.addEventListener('keydown', function (e) {
+    var game = new Game();
+    document.addEventListener("keydown", function (e) {
         switch (e.key) {
-            case ('ArrowUp'):
+            case "ArrowUp":
                 game.rotatePiece();
                 break;
-            case ('ArrowLeft'):
-                game.movePiece('left');
+            case "ArrowLeft":
+                game.movePiece("left");
                 break;
-            case ('ArrowRight'):
-                game.movePiece('right');
+            case "ArrowRight":
+                game.movePiece("right");
                 break;
-            case ('ArrowDown'):
+            case "ArrowDown":
                 game.bigDrop();
                 break;
         }
@@ -239,25 +216,25 @@ function browserGame() {
 }
 function consoleGame() {
     var game = new Game();
-    var readline = require('readline');
+    var readline = require("readline");
     readline.emitKeypressEvents(process.stdin);
     process.stdin.setRawMode(true);
-    process.stdin.on('keypress', function (str, key) {
-        if (key.ctrl && key.name === 'c') {
+    process.stdin.on("keypress", function (str, key) {
+        if (key.ctrl && key.name === "c") {
             process.exit();
         }
         else {
             switch (key.name) {
-                case ('up'):
+                case "up":
                     game.rotatePiece();
                     break;
-                case ('left'):
-                    game.movePiece('left');
+                case "left":
+                    game.movePiece("left");
                     break;
-                case ('right'):
-                    game.movePiece('right');
+                case "right":
+                    game.movePiece("right");
                     break;
-                case ('down'):
+                case "down":
                     game.bigDrop();
                     break;
             }
@@ -269,4 +246,4 @@ function consoleGame() {
     };
     var runGame = setInterval(tick, game.getInterval() * 100);
 }
-browserGame();
+consoleGame();
--- tetrocity.ts
+++ tetrocity.ts
@@ -8,42 +8,13 @@ interface Shape {
 }
 
 const SHAPES: Shape[] = [
-  { type: "square",
-    diagram: ["11", "11"],
-  },
-  { type: "rightL",
-    diagram: ["1.", "1.", "11"],
-  },
-  { type: "leftL",
-    diagram: 
-    [".1",
-    ".1",
-    "11",
-    ]
-  },
-  { type: 'rightZ',
-    diagram: 
-    ["1.",
-    "11",
-    ".1",
-    ]
-  },
-  { type: "leftZ",
-    diagram: 
-    [".1",
-      "11",
-      "1."
-    ]
-  },
-  {type: 'line',
-    diagram: ["1", "1", "1", "1"],
-  },
-  {type: 'prong',
-  diagram: [
-    ".1.",
-    "111",
-    ],
-  }
+  { type: "square", diagram: ["11", "11"] },
+  { type: "rightL", diagram: ["1.", "1.", "11"] },
+  { type: "leftL", diagram: [".1", ".1", "11"] },
+  { type: "rightZ", diagram: ["1.", "11", ".1"] },
+  { type: "leftZ", diagram: [".1", "11", "1."] },
+  { type: "line", diagram: ["1", "1", "1", "1"] },
+  { type: "prong", diagram: [".1.", "111"] }
 ];
 
 interface RenderableGame {
@@ -52,35 +23,33 @@ interface RenderableGame {
   isFilled(r: number, c: number): boolean;
 }
 
-function htmlRender(game: RenderableGame){
-  let gameboard = document.getElementById('gameboard');
-  gameboard.innerHTML = '';
-  for(let r = 0; r < game.getHeight(); r++) {
-    let row = document.createElement('div');
-    row.className = 'row';
-    for(let c = 0; c < game.getWidth(); c++){
-        let cell = document.createElement('div');
-        cell.className = game.isFilled(r,c) ? 'cell filled' : 'cell';
-        row.appendChild(cell);
-      }
-      gameboard.appendChild(row);
+function htmlRender(game: RenderableGame) {
+  let gameboard = document.getElementById("gameboard");
+  gameboard.innerHTML = "";
+  for (let r = 0; r < game.getHeight(); r++) {
+    let row = document.createElement("div");
+    row.className = "row";
+    for (let c = 0; c < game.getWidth(); c++) {
+      let cell = document.createElement("div");
+      cell.className = game.isFilled(r, c) ? "cell filled" : "cell";
+      row.appendChild(cell);
     }
+    gameboard.appendChild(row);
+  }
 }
 
-function consoleRender(game: RenderableGame){
-  console.log("==========================")
-  for(let r = 0; r < game.getHeight(); r++){
-    let row: string = '';
-    for(let c = 0; c < game.getWidth(); c++){
-      row += game.isFilled(r,c) ? '[]' : '00';
+function consoleRender(game: RenderableGame) {
+  console.log("==========================");
+  for (let r = 0; r < game.getHeight(); r++) {
+    let row: string = "";
+    for (let c = 0; c < game.getWidth(); c++) {
+      row += game.isFilled(r, c) ? "[]" : "00";
     }
     console.log(row);
   }
 }
 
-
 class Game implements RenderableGame {
-
   private grid: number[][];
   private interval: number;
   private totalScore: number;
@@ -104,30 +73,30 @@ class Game implements RenderableGame {
     this.shape = SHAPES[3].diagram;
   }
 
-  getHeight(){
+  getHeight() {
     return this.grid.length;
   }
 
-  getWidth(){
+  getWidth() {
     return this.grid[0].length;
   }
 
-  getInterval(){
+  getInterval() {
     return this.interval;
   }
 
-  canPieceAdvance(direction){
+  canPieceAdvance(direction: string = undefined) {
     let row = this.pieceRow;
     let col = this.pieceCol;
-    
-    switch(direction){
-      case 'down':
+
+    switch (direction) {
+      case "down":
         row++;
         break;
-      case 'left':
+      case "left":
         col--;
         break;
-      case 'right':
+      case "right":
         col++;
         break;
       default:
@@ -135,90 +104,93 @@ class Game implements RenderableGame {
     }
 
     //boundary check
-    if(row + this.shape.length > HEIGHT || col < 0 || col + this.shape[0].length > WIDTH){
+    if (
+      row + this.shape.length > HEIGHT ||
+      col < 0 ||
+      col + this.shape[0].length > WIDTH
+    ) {
       return false;
     }
 
-    for(let r = 0; r < this.shape.length; r++){
-      for(let c = 0; c < this.shape[0].length; c++){
-        if(this.grid[row + r][col + c] === 1 && this.shape[r][c] === '1'){
+    for (let r = 0; r < this.shape.length; r++) {
+      for (let c = 0; c < this.shape[0].length; c++) {
+        if (this.grid[row + r][col + c] === 1 && this.shape[r][c] === "1") {
           return false;
-        } 
+        }
       }
     }
     return true;
   }
 
-  fill(){
-    for(let r = 0; r < this.shape.length; r++){
-      for(let c = 0; c < this.shape[0].length; c++){
-        if(this.shape[r][c] === '1'){
+  fill() {
+    for (let r = 0; r < this.shape.length; r++) {
+      for (let c = 0; c < this.shape[0].length; c++) {
+        if (this.shape[r][c] === "1") {
           this.grid[this.pieceRow + r][this.pieceCol + c] = 1;
         }
       }
     }
     const rowsCleared = this.clearRows();
-    if(rowsCleared > 0) this.totalScore++;
+    if (rowsCleared > 0) this.totalScore++;
     this.pieceRow = 0;
     this.pieceCol = STARTING_POSITION;
     this.shape = SHAPES[Math.floor(Math.random() * SHAPES.length)].diagram;
   }
 
-  isFilled(r: number,c: number): boolean {
-    if (this.grid[r][c] === 1) return true
-    if(r >= this.pieceRow && r < this.pieceRow + this.shape.length){
-      if(c >= this.pieceCol && c < this.pieceCol + this.shape[0].length){
-        return this.shape[r - this.pieceRow][c - this.pieceCol] === '1'
+  isFilled(r: number, c: number): boolean {
+    if (this.grid[r][c] === 1) return true;
+    if (r >= this.pieceRow && r < this.pieceRow + this.shape.length) {
+      if (c >= this.pieceCol && c < this.pieceCol + this.shape[0].length) {
+        return this.shape[r - this.pieceRow][c - this.pieceCol] === "1";
       }
     }
-    return false
+    return false;
   }
 
-  rotatePiece(){ 
+  rotatePiece() {
     // Currently this only rotates to the right
     let oldShape = [...this.shape];
     let newShape = [];
-    while(oldShape[0] !== ''){
-      let newShapeLine = '';
-      for(let r = oldShape.length - 1; r > -1; r--){
+    while (oldShape[0] !== "") {
+      let newShapeLine = "";
+      for (let r = oldShape.length - 1; r > -1; r--) {
         newShapeLine += oldShape[r][0];
         oldShape[r] = oldShape[r].slice(1);
       }
       newShape.push(newShapeLine);
     }
     this.shape = newShape;
-    if((this.pieceCol + this.shape[0].length) >= WIDTH - 1){
+    if (this.pieceCol + this.shape[0].length >= WIDTH - 1) {
       this.pieceCol = WIDTH - this.shape[0].length;
     }
-    if(!this.canPieceAdvance(null)) this.shape = oldShape;
+    if (!this.canPieceAdvance()) this.shape = oldShape;
   }
 
-  bigDrop(){
-    while(this.canPieceAdvance('down')){
+  bigDrop() {
+    while (this.canPieceAdvance("down")) {
       this.pieceRow++;
     }
     this.fill();
   }
 
   dropPiece() {
-    if(this.canPieceAdvance('down')){
+    if (this.canPieceAdvance("down")) {
       this.pieceRow++;
-    } 
-    else {
+    } else {
       this.fill();
     }
   }
 
   movePiece(direction: string) {
-    if(this.canPieceAdvance(direction)){
-      switch(direction){
-        case 'down':
+    if (this.canPieceAdvance(direction)) {
+      switch (direction) {
+        case "down":
           this.pieceRow++;
           break;
-        case 'left':
+        case "left":
           this.pieceCol--;
           break;
-        case 'right':
+        case "right":
           this.pieceCol++;
           break;
         default:
@@ -227,77 +199,76 @@ class Game implements RenderableGame {
     }
   }
 
-  turn(){
+  turn() {
     this.dropPiece();
   }
 
   areYouDead() {
-    return this.grid[0].indexOf(1) !== -1
+    return this.grid[0].indexOf(1) !== -1;
   }
 
-  clearRows(){
+  clearRows() {
     let counter = 0;
-    this.grid.forEach((row,i) => {
-      if(row.every(char => char === 1)){
+    this.grid.forEach((row, i) => {
+      if (row.every(char => char === 1)) {
         counter++;
         const newRow = [];
-        for(let i = 0; i < WIDTH; i++){
+        for (let i = 0; i < WIDTH; i++) {
           newRow.push(0);
         }
-        this.grid.splice(i,1);
-        this.grid.splice(0,0,newRow);
+        this.grid.splice(i, 1);
+        this.grid.splice(0, 0, newRow);
       }
     });
-    return counter; 
+    return counter;
   }
 }
 
-function browserGame(){
-  const game = new Game;
-  document.addEventListener('keydown', e => {
-          switch(e.key){
-            case('ArrowUp'):
-              game.rotatePiece();
-              break;
-            case('ArrowLeft'):
-              game.movePiece('left');
-              break;
-            case('ArrowRight'):
-              game.movePiece('right');
-              break;
-            case('ArrowDown'):
-              game.bigDrop();
-              break;
-          }
-        })
+function browserGame() {
+  const game = new Game();
+  document.addEventListener("keydown", e => {
+    switch (e.key) {
+      case "ArrowUp":
+        game.rotatePiece();
+        break;
+      case "ArrowLeft":
+        game.movePiece("left");
+        break;
+      case "ArrowRight":
+        game.movePiece("right");
+        break;
+      case "ArrowDown":
+        game.bigDrop();
+        break;
+    }
+  });
   const tick = () => {
     game.turn();
     htmlRender(game);
-  }
+  };
   const gameTime = setInterval(tick, game.getInterval() * 100);
 }
 
-function consoleGame(){
+function consoleGame() {
   const game = new Game();
-  const readline = require('readline');
+  const readline = require("readline");
   readline.emitKeypressEvents(process.stdin);
   process.stdin.setRawMode(true);
-  process.stdin.on('keypress', (str,key) => {
-    if(key.ctrl && key.name === 'c'){
+  process.stdin.on("keypress", (str, key) => {
+    if (key.ctrl && key.name === "c") {
       process.exit();
-    }
-    else {
-      switch(key.name){
-        case('up'):
+    } else {
+      switch (key.name) {
+        case "up":
           game.rotatePiece();
           break;
-        case('left'):
-          game.movePiece('left');
+        case "left":
+          game.movePiece("left");
           break;
-        case('right'):
-          game.movePiece('right');
+        case "right":
+          game.movePiece("right");
           break;
-        case('down'):
+        case "down":
           game.bigDrop();
           break;
       }
@@ -306,10 +277,8 @@ function consoleGame(){
   const tick = () => {
     game.turn();
     consoleRender(game);
-  }
+  };
   const runGame = setInterval(tick, game.getInterval() * 100);
 }
 
-browserGame();
-
-
+consoleGame();
